4d5354699bcafdebe02c1c3356669174cdca19aa
Daniel Johansson <dannesnake@gmail.com>
2024-09-24 11:29:34 +0200

    Create new component for adding Book to database

---
+++ src/app/createbook/createbook.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createbook',
+  standalone: true,
+  imports: [],
+  templateUrl: './createbook.component.html',
+  styleUrl: './createbook.component.css'
+})
+export class CreatebookComponent {
+
+}
