659a57e04a0b4d9815e232c1113cbdff57ef5e4b
Daniel Johansson <dannesnake@gmail.com>
2024-09-24 11:31:03 +0200

    Add route and link to createbook.component in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BookSearchComponent } from './book-search/book-search.component';
 import { BooklistDisplayComponent } from './booklist-display/booklist-display.component';
+import { CreatebookComponent } from './createbook/createbook.component';
 
 export const routes: Routes = [
     { path: 'book-search-component', component: BookSearchComponent },
-    { path: 'booklist-display-component', component: BooklistDisplayComponent }
+    { path: 'booklist-display-component', component: BooklistDisplayComponent },
+    { path: 'createbook-component', component: CreatebookComponent }
 ];
