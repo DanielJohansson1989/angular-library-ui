169e9f6950b6b7f5ce3d1169bdf5ffe834bec0b3
Daniel Johansson <dannesnake@gmail.com>
2024-09-25 18:13:28 +0200

    Create component with a form to generate a new book entry in database

--- src/app/createbook/createbook.component.ts
+++ src/app/createbook/createbook.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { APIResponseService } from '../service/apiresponse.service';
+import { APIResponse } from '../models/apiresponse';
+import { FormsModule } from '@angular/forms';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-createbook',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './createbook.component.html',
   styleUrl: './createbook.component.css'
 })
 export class CreatebookComponent {
+  book: Book = {
+    iD: 0,
+    title: '',
+    author: '',
+    dateOfPublication: '',
+    genre: '',
+    imageURL: '',
+    description: '',
+    available: true
+  };
 
+  constructor( private apiResponseService: APIResponseService) {}
+
+  onSubmit(){
+    this.apiResponseService.createBook(this.book).subscribe({
+      next: (response: APIResponse<Book>) => {
+        console.log(response.result);
+        if (response.isSuccess){
+          this.resetForm();
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching data: ', error);
+      }
+    });
+  }
+
+  resetForm() {
+    this.book = {
+      iD: 0,
+      title: '',
+      author: '',
+      dateOfPublication: '',
+      genre: '',
+      imageURL: '',
+      description: '',
+      available: true
+    }
+  }
 }
