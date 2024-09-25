dc699dac6c15d8f654e329d19cba3d1fa406cef7
Daniel Johansson <dannesnake@gmail.com>
2024-09-25 17:47:38 +0200

    Remove old code that is not used The code was written when I first tried to call the API and now the code is moved to a separate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { APIResponse } from './models/apiresponse';
-import { APIResponseService } from './service/apiresponse.service';
-import { response } from 'express';
-import { error } from 'console';
 import { CommonModule } from '@angular/common';
-import { HttpStatusCode } from '@angular/common/http';
 import { HeaderComponent } from "./header/header.component";
 
 @Component({
@@ -17,30 +12,4 @@ import { HeaderComponent } from "./header/header.component";
 })
 export class AppComponent {
   title = 'angular-library-ui';
-
-  apiResponse: APIResponse = {
-    isSuccess: true,
-    result: [],
-    httpStatusCode: HttpStatusCode.Ok,
-    errorList:[]
-  }
-
-  constructor (private apiResponseService: APIResponseService){
-
-  }
-  ngOnInit(): void{
-    this.getApiResponse();
-  }
-
-  getApiResponse(){
-    this.apiResponseService.getAllBooks().subscribe({
-      next: (response: APIResponse) => {
-        this.apiResponse = response;
-        console.log('API Response: ', this.apiResponse);
-      },
-      error: (error) => {
-        console.error('Error fetching data: ', error);
-      }
-    });
-  }
 }
